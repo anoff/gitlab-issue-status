73bb6621c6d5d2fb8d517e8123c3037949b03c89
anoff <offenhaeuser@gmail.com>
2018-06-07 22:07:27 +0200

    :star2: feat: add error icon

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 require('dotenv').config()
-const {tagToUrl, UrlToTag, getIssue} = require('.').gitlab
-const {startServer} = require('.').server
-const {loadIcons, createImage} = require('.').canvas
-
-const baseUrl = process.env.GITLAB_API_URL
-const token = process.env.GITLAB_ACCESS_KEY
-
-const port = process.env.PORT || 8080
+const {tagToUrl, UrlToTag, getIssue} = require('./lib/gitlab')
+const {startServer} = require('./lib/server')
+const {loadIcons, createImage} = require('./lib/canvas')
 
 const iconArray = [
   {key: 'closed', path: './assets/check.png'},
   {key: 'opened', path: './assets/cross.png'},
-  {key: 'tool', path: './assets/tool.png'}
+  {key: 'tool', path: './assets/tool.png'},
+  {key: 'error', path: './assets/fire.png'}
 ]
 
 let icons
 
-function init () {
-  return loadIcons(iconArray)
+function init (opts = {}) {
+  opts.iconArray = opts.iconArray || iconArray
+  opts.baseUrl = process.env.GITLAB_API_URL || opts.baseUrl
+  opts.token = process.env.GITLAB_ACCESS_KEY || opts.token
+  opts.port = process.env.PORT || opts.port || 8080
+  return loadIcons(opts.iconArray)
     .then(res => {
       icons = res
     })
-    .then(() => startServer(port, (issueUrl, res) => {
+    .then(() => startServer(opts.port, (issueUrl, res) => {
       const issueTag = UrlToTag(issueUrl)
-      const fullUrl = tagToUrl(issueTag, baseUrl)
+      const fullUrl = tagToUrl(issueTag, opts.baseUrl)
       console.log(`Creating issue for ${issueTag} at URL: ${fullUrl}`)
-      return getIssue(fullUrl, token)
+      return getIssue(fullUrl, opts.token)
         .then(issue => {
           return createImage(issue, icons)
         })
     }))
     .then(server => {
-      console.log(`Server started on port ${port}`)
+      console.log(`Server started on port ${opts.port}`)
     })
 }
 
