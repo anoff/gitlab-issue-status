e11b3195c391c34df84f17932755158c2771b99f
Andreas Offenhaeuser <anoff@users.noreply.github.com>
2018-08-22 09:52:19 +0200

    resolve cyclic module dependency

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
 require('dotenv').config()
-const {tagToUrl, UrlToTag, getIssue} = require('./lib/gitlab')
-const {startServer} = require('./lib/server')
-const {loadIcons, createImage} = require('./lib/canvas')
 
-const iconArray = [
-  {key: 'closed', path: './assets/check.png'},
-  {key: 'opened', path: './assets/cross.png'},
-  {key: 'tool', path: './assets/tool.png'},
-  {key: 'error', path: './assets/fire.png'}
-]
+module.exports = function app ({gitlab, canvas, server}) {
+  const {tagToUrl, UrlToTag, getIssue} = gitlab
+  const {startServer} = server
+  const {loadIcons, createImage} = canvas
+  
+  const baseUrl = process.env.GITLAB_API_URL
+  const token = process.env.GITLAB_ACCESS_KEY
+  
+  const port = process.env.PORT || 8080
+  
+  const iconArray = [
+    {key: 'closed', path: './assets/check.png'},
+    {key: 'opened', path: './assets/cross.png'},
+    {key: 'tool', path: './assets/tool.png'}
+  ]
+  
+  let icons
+  
+  function init () {
+    return loadIcons(iconArray)
+      .then(res => {
+        icons = res
+      })
+      .then(() => startServer(port, (issueUrl, res) => {
+        const issueTag = UrlToTag(issueUrl)
+        const fullUrl = tagToUrl(issueTag, baseUrl)
+        console.log(`Creating issue for ${issueTag} at URL: ${fullUrl}`)
+        return getIssue(fullUrl, token)
+          .then(issue => {
+            return createImage(issue, icons)
+          })
+      }))
+      .then(server => {
+        console.log(`Server started on port ${port}`)
+      })
+  }
 
-let icons
-
-function init (opts = {}) {
-  opts.iconArray = opts.iconArray || iconArray
-  opts.baseUrl = process.env.GITLAB_API_URL || opts.baseUrl
-  opts.token = process.env.GITLAB_ACCESS_KEY || opts.token
-  opts.port = process.env.PORT || opts.port || 8080
-  return loadIcons(opts.iconArray)
-    .then(res => {
-      icons = res
-    })
-    .then(() => startServer(opts.port, (issueUrl, res) => {
-      const issueTag = UrlToTag(issueUrl)
-      const fullUrl = tagToUrl(issueTag, opts.baseUrl)
-      console.log(`Creating issue for ${issueTag} at URL: ${fullUrl}`)
-      return getIssue(fullUrl, opts.token)
-        .then(issue => {
-          return createImage(issue, icons, opts)
-        })
-    }))
-    .then(server => {
-      console.log(`Server started on port ${opts.port}`)
-    })
+  return init
 }
-
-module.exports = init
